4055a0687fb4f9b309125166cdc0459839abe8c9
Manali Darji <darjimanali93@gmail.com>
2022-04-12 15:12:36 -0400

    implement read single property

--- src/main/java/com/example/restinnAPI/controller/PropertyController.java
+++ src/main/java/com/example/restinnAPI/controller/PropertyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class PropertyController {
 	@Autowired
 	private PropertyService propServiceObj;
 	
+	// for reading all properties
 	// @GetMapping handles the HTTP GET requests matched with given URI expression.
 	@GetMapping("/property")
 	public ResponseEntity<List<PropertyModel>> getAllProperties(){
@@ -31,7 +33,19 @@ public class PropertyController {
 		}
 		return new ResponseEntity<List<PropertyModel>>(allProps, HttpStatus.OK);
 	}
+	
+	// for reading single property
+	// @PathVariable: Annotation which indicates that a method parameter should be bound to a URI template variable.
+	@GetMapping("/property/{propId}")
+	public ResponseEntity<PropertyModel> getSingleProperty(@PathVariable String propId){
+		PropertyModel singleProp = propServiceObj.getSingleProperty(propId);
+		if(singleProp == null) {
+			return new ResponseEntity<PropertyModel>(new PropertyModel(), HttpStatus.NOT_FOUND);		
+		}
+		return new ResponseEntity<PropertyModel>(singleProp, HttpStatus.OK);
+	}
 
+	// for creating new property
 	// @PostMapping handles the HTTP POST requests matched with given URI expression.
 	@PostMapping(value = "/property", consumes = {
 			MediaType.APPLICATION_JSON_VALUE
--- src/main/java/com/example/restinnAPI/service/PropertyService.java
+++ src/main/java/com/example/restinnAPI/service/PropertyService.java
@@ -1,6 +1,7 @@
 package com.example.restinnAPI.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,15 +15,25 @@ import com.example.restinnAPI.repository.PropertyDao;
 public class PropertyService {
 	// dependency injection as per singleton spring design pattern, only one object across the whole application
 	@Autowired
-	PropertyDao propertyDaoObj;
+	PropertyDao propertyDaoObj;	
 
+	// read all properties from DB
+	public List<PropertyModel> getAllProperties() {
+		return propertyDaoObj.findAll();
+	}
+
+	// read single property from DB
+	public PropertyModel getSingleProperty(String propId) {
+		Optional<PropertyModel> singleProp = propertyDaoObj.findById(propId);
+		if(singleProp.isPresent()) {
+			return singleProp.get();
+		}
+		return null;
+	}
+	
 	// create new property in DB
 	public PropertyModel createProperty(PropertyModel property) {
 		PropertyModel newPropCreated = propertyDaoObj.insert(property);
 		return newPropCreated;
-	}
-
-	public List<PropertyModel> getAllProperties() {
-		return propertyDaoObj.findAll();
 	}
 }
