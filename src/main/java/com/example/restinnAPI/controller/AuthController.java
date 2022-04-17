6768d8c4f416923a2e666d606167684c72b8b3e0
Manali Darji <darjimanali93@gmail.com>
2022-04-17 16:12:01 -0400

    fix cross origin issue to connect to front end

--- src/main/java/com/example/restinnAPI/controller/AuthController.java
+++ src/main/java/com/example/restinnAPI/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.restinnAPI.model.UserModel;
 import com.example.restinnAPI.repository.UserDao;
 
+@CrossOrigin(origins = "https://rest-inn-api-mvdarji-spring.herokuapp.com")
 @RestController
 public class AuthController {
 	@Autowired
--- src/main/java/com/example/restinnAPI/controller/PropertyController.java
+++ src/main/java/com/example/restinnAPI/controller/PropertyController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.restinnAPI.model.PropertyModel;
 import com.example.restinnAPI.service.PropertyService;
 
+@CrossOrigin(origins = "https://rest-inn-api-mvdarji-spring.herokuapp.com")
 // In Spring's approach to building RESTful web services, HTTP requests are handled by a controller. 
 // These components are identified by the @RestController annotation
 @RestController
